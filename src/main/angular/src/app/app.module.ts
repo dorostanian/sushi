0e7cf45002a7600e6d5625044fb926d53e17deb8
rcijvat <rcijvat@bol.com>
2018-12-22 20:13:04 +0100

    Setup initial components

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
+import { GraphEditorComponent } from './components/graph-editor/graph-editor.component';
+import { ShellOutputComponent } from './components/shell-output/shell-output.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeEditorComponent,
+    GraphEditorComponent,
+    ShellOutputComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
