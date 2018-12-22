027244ed519ee836df2be506f21e9eeed9b9c7bd
rcijvat <rcijvat@bol.com>
2018-12-22 20:49:56 +0100

    Upgrade to angular 7

---
+++ src/main/angular/src/app/components/shell-output/shell-output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shell-output',
+  templateUrl: './shell-output.component.html',
+  styleUrls: ['./shell-output.component.scss']
+})
+export class ShellOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
